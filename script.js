89886700ea2b211abd5416c2add5dd657e9f1a99
igcarvalhaes <igcarvalhaes@hotmail.com>
2024-08-29 16:31:58 -0300

    Feat: function the loops through the array

--- script.js
+++ script.js
@@ -5,3 +5,21 @@ function Book(title, author, pages) {
   this.author = author;
   this.pages = pages;
 }
+
+const lordOfTheRings = new Book("Lord of The Rings", "Tolkien", 400);
+const harryPotter = new Book("Harry Potter 1", "JK Rowling", 599);
+
+myLibrary.push(lordOfTheRings);
+myLibrary.push(harryPotter);
+
+function showBooks(library) {
+  return library.map((book) => {
+    return {
+      title: book.title,
+      author: book.author,
+      pages: book.pages,
+    };
+  });
+}
+
+console.log(showBooks(myLibrary));
