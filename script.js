28d9dec8c87440bef0a133b970f911408850d5df
igcarvalhaes <igcarvalhaes@hotmail.com>
2024-08-29 17:49:06 -0300

    Feat: function that displays all book cards

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+const main = document.querySelector("main");
+
 const myLibrary = [];
 
 function Book(title, author, pages) {
@@ -7,19 +9,26 @@ function Book(title, author, pages) {
 }
 
 const lordOfTheRings = new Book("Lord of The Rings", "Tolkien", 400);
-const harryPotter = new Book("Harry Potter 1", "JK Rowling", 599);
+const harryPotter = new Book("Harry Potter", "JK Rowling", 599);
+const crepusculo = new Book("Crepusculo", "Sei la", 200);
 
 myLibrary.push(lordOfTheRings);
 myLibrary.push(harryPotter);
+myLibrary.push(crepusculo);
 
-function showBooks(library) {
-  return library.map((book) => {
-    return {
-      title: book.title,
-      author: book.author,
-      pages: book.pages,
-    };
+function mostrarLivros(library) {
+  library.forEach((book) => {
+    const bookCard = document.createElement("div");
+    bookCard.classList.add("book-card");
+
+    bookCard.innerHTML = `
+        <div class="book-title">${book.title}</div>
+        <div class="book-author">${book.author}</div>
+        <div class="pages">${book.pages} Pages</div>
+      `;
+
+    main.appendChild(bookCard);
   });
 }
 
-console.log(showBooks(myLibrary));
+mostrarLivros(myLibrary);
